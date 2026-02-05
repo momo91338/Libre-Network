3aeaea9aaf9646764889b477a28b0ebe77bde64d
momo91338 <abohash1991@gmail.com>
2026-02-05 19:08:55 +0100

    Delete libre-network directory

--- libre-network/crates/core/src/miner.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Miner {
-    pub address: [u8; 32],
-    pub public_key: [u8; 32],
-    pub mining_age: u64,
-}
--- libre-network/crates/ui/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod app;
-pub mod wallet_page;
-pub mod mining_page;
-pub mod network_status_page;
-pub mod settings_page;
